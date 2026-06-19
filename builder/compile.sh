a9e28c6addc0d6696989d83ddf3a08551ececb53
sflvrr <simon.khodus@yandex.ru>
2026-06-19 23:21:08 +0200

    вроде всё

--- builder/compile.sh
+++ builder/compile.sh
@@ -3,9 +3,9 @@ set -e
 
 cd /workspace/src
 
-pdflatex -interaction=batchmode -halt-on-error resume.tex
-pdflatex -interaction=batchmode -halt-on-error resume.tex
+pdflatex -interaction=nonstopmode -halt-on-error resume.tex
+pdflatex -interaction=nonstopmode -halt-on-error resume.tex
 
-mv resume.pdf /workspace/Semyon_Khodus_CV.pdf
+mv resume.pdf /workspace/CV.pdf
 
 rm -f *.aux *.log *.out *.toc *.fls *.fdb_latexmk
