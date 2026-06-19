d2be1084bdcf79375d455c21702c3b66847bd428
sflvrr <simon.khodus@yandex.ru>
2026-06-19 23:30:20 +0200

    вроде всё

--- builder/compile.sh
+++ builder/compile.sh
@@ -6,6 +6,6 @@ cd /workspace/src
 pdflatex -interaction=nonstopmode -halt-on-error CV.tex
 pdflatex -interaction=nonstopmode -halt-on-error CV.tex
 
-mv resume.pdf /workspace/CV.pdf
+mv CV.pdf /workspace/CV.pdf
 
 rm -f *.aux *.log *.out *.toc *.fls *.fdb_latexmk
